067ec39b0fb6619f82f0d81aa6b64c2d6fa7a919
vladmeh <vladmeh@gmail.com>
2019-10-14 00:22:25 +0300

    life cycle events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1 v-text="sitename"></h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    let APP_LOG_LIFECYCLE_EVENTS = true;
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                sitename: 'Vue.js Pet Depot'
+            }
+        },
+        beforeCreate() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("beforeCreated");
+            }
+        },
+        created() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("created");
+            }
+        },
+        beforeMount() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("beforeMount");
+            }
+        },
+        mounted() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("mounted");
+            }
+        },
+        beforeUpdate() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("beforeUpdate");
+            }
+        },
+        updated() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("updated");
+            }
+        },
+        beforeDestroy() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("beforeDestroy");
+            }
+        },
+        destroyed() {
+            if (APP_LOG_LIFECYCLE_EVENTS){
+                console.log("destroyed");
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
