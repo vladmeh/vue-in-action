091be24fbc1199067556fa236047ac9cdd3f4dfa
vladmeh <vladmeh@gmail.com>
2019-10-27 01:54:35 +0300

    Chapter 10. Vuex modules.

--- petstore/src/main.js
+++ petstore/src/main.js
@@ -2,32 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.scss'
-import { store } from './store/store';
+import {store} from './store/store';
 
 Vue.config.productionTip = false;
 
 Vue.filter('formatPrice', function (price) {
-  if (!parseInt(price)) {
-    return "";
-  }
-
-  if (price > 99999) {
-    let priceString = (price / 100).toFixed(2);
-    let priceArray = priceString.split("").reverse();
-    let index = 3;
-    while (priceArray.length > index + 3) {
-      priceArray.splice(index + 3, 0, ",");
-      index += 4;
+    if (!parseInt(price)) {
+        return "";
     }
-    return "$" + priceArray.reverse().join("");
-  } else {
-    return "$" + (price / 100).toFixed(2);
-  }
+
+    if (price > 99999) {
+        let priceString = (price / 100).toFixed(2);
+        let priceArray = priceString.split("").reverse();
+        let index = 3;
+        while (priceArray.length > index + 3) {
+            priceArray.splice(index + 3, 0, ",");
+            index += 4;
+        }
+        return "$" + priceArray.reverse().join("");
+    } else {
+        return "$" + (price / 100).toFixed(2);
+    }
 });
 
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app');
